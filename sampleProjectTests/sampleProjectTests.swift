0c7c6ff7c8e3d0a03f3625e95c643fc94206c9d5
ANGUNATHAN_M <angunathan_m@MM-ANGUNATHAN-M.local>
2017-03-03 20:21:03 +0530

    first commit

---
+++ sampleProjectTests/sampleProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  sampleProjectTests.swift
+//  sampleProjectTests
+//
+//  Created by Angunathan Masanam on 03/03/17.
+//  Copyright © 2017 ANGUNATHAN_M. All rights reserved.
+//
+
+import XCTest
+@testable import sampleProject
+
+class sampleProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
